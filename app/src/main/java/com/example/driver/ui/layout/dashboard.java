699e0473af061bc2fa8a896a628bf40d264ce3b8
dilshan34 <dilshansachinthana34@gmail.com>
2021-09-21 19:05:49 +0530

    start vehicle notification

--- app/src/main/java/com/example/driver/ui/layout/dashboard.java
+++ app/src/main/java/com/example/driver/ui/layout/dashboard.java
@@ -6,17 +6,33 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.driver.R;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import Driver.MapsActivity;
 import User.UserMapActivity;
 
 public class dashboard extends AppCompatActivity {
 
-    private DrawerLayout drawer;
     CardView user,driver;
+    private RequestQueue mRequestQue;
+    private String URL = "https://fcm.googleapis.com/fcm/send";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +41,9 @@ public class dashboard extends AppCompatActivity {
 
         user = findViewById(R.id.user);
         driver = findViewById(R.id.driver);
+
+        mRequestQue = Volley.newRequestQueue(this);
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
 
         user.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,12 +57,58 @@ public class dashboard extends AppCompatActivity {
         driver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sendNotification();
                 Intent intent = new Intent(dashboard.this, MapsActivity.class);
                 startActivity(intent);
+
             }
         });
 
 
 
+    }
+
+    public void sendNotification(){
+        JSONObject mainObj = new JSONObject();
+        try {
+            mainObj.put("to","/topics/"+"news");
+            JSONObject notificationObj = new JSONObject();
+            notificationObj.put("title","Taxi");
+            notificationObj.put("body","Vehicle is start from the destination");
+
+            mainObj.put("notification",notificationObj);
+
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, URL,
+                    mainObj,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+
+                            Log.d("MUR", "onResponse: ");
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("MUR", "onError: "+error.networkResponse);
+                }
+            }
+            ){
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String,String> header = new HashMap<>();
+                    header.put("content-type","application/json");
+                    header.put("authorization","key=AAAArHo2bVo:APA91bH6ezUoeT4mXRjOsTujQnte-K5mWrnXKC8S8GatUJD-RDGJcA9qI64ZCkl-mA2_UsbITWdREMOfCaI5vnud7xo7ls2L07AB-xF8qYtmjU4s13VtQ2XtFdpIoRwtCOeScFoFr0pE");
+                    Log.e("TAG", "getHeaders: "+header );
+                    return header;
+                }
+            };
+            mRequestQue.add(request);
+        }
+        catch (JSONException e)
+
+        {
+            e.printStackTrace();
+        }
     }
 }
